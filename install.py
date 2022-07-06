804b94718ec203455c049420f2c2a91eeca4596d
Chris <chrisfishbob@gmail.com>
2022-07-06 11:30:26 -0700

    Sudo added to chmod command

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     os.system("sudo cp ocl.py /usr/local/bin/ocl")
-    os.system("chmod +x /usr/local/bin/ocl")
+    os.system("sudo chmod +x /usr/local/bin/ocl")
     print("Installation complete")
 
 if __name__ == "__main__":
